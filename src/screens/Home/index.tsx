e368250c7f2b6d36966edfac1b8d75179e843e07
LucasAlencar <lucas.linard@outlook.com>
2023-03-09 10:49:23 -0300

    created form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, Touchable, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
 
@@ -7,11 +7,22 @@ export default function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Home</Text>
       <Text style={styles.eventDate}>Home</Text>
+      <View style={styles.form}>
       <TextInput 
       style={styles.input}
       placeholder='Nome do participante'
       placeholderTextColor='#6b6b6b'
       />
+
+      <TouchableOpacity 
+      style={styles.button}
+      onPress={() => {}}
+      >
+        <Text style={styles.buttonText}>
+          +
+        </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
