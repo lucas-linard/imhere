335a21f75f9ac39abfcab9895b170ae011cb382e
LucasAlencar <lucas.linard@outlook.com>
2023-03-09 10:49:37 -0300

    created form styles

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -12,15 +12,35 @@ export const styles = StyleSheet.create({
     fontWeight: "bold",
     marginTop: 48,
   },
-  eventDate:{
-    color: '#6B6B6B',
+  eventDate: {
+    color: "#6B6B6B",
     fontSize: 16,
   },
-  input:{
+  input: {
+    flex: 1,
     height: 56,
-    backgroundColor: '#1F1E25',
+    backgroundColor: "#1F1E25",
     borderRadius: 8,
-    color: '#FFF',
+    color: "#FFF",
     padding: 16,
-  }
+    marginRight: 12,
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 24,
+  },
+  button: {
+    backgroundColor: "#31CF67",
+    borderRadius: 5,
+    width: 56,
+    height: 56,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 36,
+    marginBottom: 42,
+  },
 });
