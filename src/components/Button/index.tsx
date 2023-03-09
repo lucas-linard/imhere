c369244e42709fbf3307062ae53a2fe8b8a82cac
LucasAlencar <lucas.linard@outlook.com>
2023-03-09 14:45:31 -0300

    button onPress fixed

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,7 @@ import { styles } from "./styles";
 
 export function Button(props: { onPress?: () => void , title?: string, style?: object}) {
   return (
-    <TouchableOpacity style={[styles.button, props.style]} onPress={() => props.onPress}>
+    <TouchableOpacity style={[styles.button, props.style]} onPress={props.onPress}>
       <Text style={styles.buttonText}>{props.title}</Text>
     </TouchableOpacity>
   );
