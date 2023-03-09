17df660acf4dc07ab469dafe39584c04719e3d95
LucasAlencar <lucas.linard@outlook.com>
2023-03-09 14:07:43 -0300

    style for emptylist

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -31,4 +31,9 @@ export const styles = StyleSheet.create({
     marginTop: 36,
     marginBottom: 42,
   },
+  listEmptyText:{
+    colort : 'FFF',
+    fontSize: 16,
+    textAlign: 'center',
+  }
 });
